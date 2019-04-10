fc7b3e17fca3ea045ba2501cd4a7be5d5c87e6df
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-04-10 18:26:32 +0530

    Chnages in first tab

--- src/app/hotels/hotels.page.ts
+++ src/app/hotels/hotels.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { HotelsService } from './hotels.service';
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.page.html',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelsPage implements OnInit {
 
-  constructor() { }
+  cart = [];
+  items = [];
+ 
+  sliderConfig = {
+    slidesPerView: 1.6,
+    spaceBetween: 10,
+    centeredSlides: true
+  };
+
+  constructor(private router: Router, private _hotelService : HotelsService) { }
 
   ngOnInit() {
+    this.items = this._hotelService.getProducts();
+    this.cart = this._hotelService.getCart();
+  }
+
+  slideOpts= {
+    loop: true, // have to add loop = true, otherwise when reach to last slide if will all the way back to first, not just smooth slide to first
+    };
+    slidesDidLoad(slides:IonSlides) {
+    slides.startAutoplay();
+    }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 600);
+  }
+
+  addToCart(product) {
+    this._hotelService.addProduct(product);
+  }
+ 
+  openCart() {
+    this.router.navigate(['cart']);
   }
 
 }
