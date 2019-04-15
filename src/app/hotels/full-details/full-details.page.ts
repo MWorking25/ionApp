504e02e33902b145e8337b4ec811c6ab60487203
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-04-15 18:12:27 +0530

    Add new page full description and done chamen= changes

---
+++ src/app/hotels/full-details/full-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-details',
+  templateUrl: './full-details.page.html',
+  styleUrls: ['./full-details.page.scss'],
+})
+export class FullDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
